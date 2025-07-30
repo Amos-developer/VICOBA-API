e857aeb7c382e9232ca5f2ecad823b98c3ed1d2a
Amos-developer <amosnyoni185@gmail.com>
2025-07-30 17:06:45 +0300

    Creating User entity and implement it

--- src/main/java/com/vocoba/vicobaapi/Entity/User.java
+++ src/main/java/com/vocoba/vicobaapi/Entity/User.java
@@ -1,5 +1,40 @@
 package com.vocoba.vicobaapi.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.management.relation.Role;
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name="users")
 public class User {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+    public enum Role {
+        MEMBER, ADMIN
+    }
+
 }
